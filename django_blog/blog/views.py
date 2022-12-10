54564a2e79063c6e64f73b1d920c2afa73a88614
Sabyasachi Mukherjee <sabyasachi.mukherjee@protonmail.com>
2022-12-10 15:35:49 +0100

    added render function and mapped to resp. html files

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse : no longer being used, hence commented out.
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('<h1> Blog Home</h1>')
+    # return HttpResponse('<h1> Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1> Blog About</>')
+    return render(request, 'blog/about.html')
+    # return HttpResponse('<h1> Blog About</h1>')
